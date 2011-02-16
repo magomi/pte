62b0dc0e3d7f7ab35364028dbef351b9f8132ccb
Marco Grunert <magomi+github@gmail.com>
2011-02-17 00:09:28 +0100

    add a demo how a slider and a textfield can be used together

--- src/main/java/de/intecsoft/PteApplication.java
+++ src/main/java/de/intecsoft/PteApplication.java
@@ -4,22 +4,19 @@ import org.odlabs.wiquery.utils.WiQueryWebApplication;
 
 /**
  * Application object for your web application. If you want to run this application without deploying, run the Start class.
- * 
- * @see de.intecsoft.Start#main(String[])
  */
 public class PteApplication extends WiQueryWebApplication {    
     /**
      * Constructor
      */
-	public PteApplication()
-	{
+	public PteApplication() {
+        mountBookmarkablePage("SliderDemo", SliderDemoPage.class);
 	}
 
     /**
 	 * @see org.apache.wicket.Application#getHomePage()
 	 */
-	public Class<PtePage> getHomePage()
-	{
+	public Class<PtePage> getHomePage() {
 		return PtePage.class;
 	}
 
